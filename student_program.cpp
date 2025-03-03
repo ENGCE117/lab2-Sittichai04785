e4c4147616f4c024f07a8882f570407a42bb3b28
Sittichai04785 <sittichai.manorat@gmail.com>
2025-03-03 15:28:48 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -4,16 +4,16 @@
 void reverse( char str1[] , char str2[]) ;
 
 int main() {
-    char text[ 50 ] = "I Love You" ;
+    char text[ 50 ] = "You and Me ." ;
     char out [50 ] ; // ouY evoL I
     reverse( text , out) ;
+    printf("%s\n" , out ) ;
     printf("%s\n" , out ) ;
     return 0 ;
 }//end function
 
 void reverse( char str1[] , char str2[] ) {
     int l = strlen(str1) ;
-    printf("%d\n" , l ) ;
     for(int i = 0 ; i < l ; i++ ) {
         str2[i] = str1[l - i - 1] ;
     }//end for
